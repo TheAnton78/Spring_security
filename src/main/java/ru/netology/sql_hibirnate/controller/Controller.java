7edea22f7ebbc0730bc67fee9dd322aa50cf1995
Shaa test <shvyrev.anton78@gmail.com>
2024-06-05 21:09:18 +0500

    initial commit

--- src/main/java/ru/netology/sql_hibirnate/controller/Controller.java
+++ src/main/java/ru/netology/sql_hibirnate/controller/Controller.java
@@ -1,5 +1,6 @@
 package ru.netology.sql_hibirnate.controller;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.sql_hibirnate.model.Person;
@@ -7,8 +8,10 @@ import ru.netology.sql_hibirnate.repository.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-@RestController
+
+@RestController("/persons")
 public class Controller {
     Repository repository;
 
@@ -16,9 +19,22 @@ public class Controller {
         this.repository = repository;
     }
 
-    @GetMapping("/persons/by-city")
+    @GetMapping("/by-city")
     public List<Person> getPersonsByCity(String city){
-        List<?> persons = repository.getPersonsByCity(city);
+        List<?> persons = repository.findByCityOfLiving(city);
         return (List<Person>) persons;
     }
+
+    @GetMapping("/less-age")
+    public List<Person> getPersonsLessAge(Integer age){
+        return repository.findByAgeLessThanOrderByAge(age);
+
+    }
+
+    @GetMapping("/by-name-surname")
+    public List<Person> getPersonsByNameSurname(String name, String surname){
+        return repository.findByNameAndSurnameLike(name, surname, Optional.empty());
+    }
+
+
 }
--- src/main/java/ru/netology/sql_hibirnate/repository/Repository.java
+++ src/main/java/ru/netology/sql_hibirnate/repository/Repository.java
@@ -2,19 +2,28 @@ package ru.netology.sql_hibirnate.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.netology.sql_hibirnate.model.CompositeKey;
 import ru.netology.sql_hibirnate.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 @org.springframework.stereotype.Repository
-public class Repository {
+public interface Repository extends JpaRepository<Person, CompositeKey> {
+    List<Person> findByCityOfLiving(String cityOfLiving);
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    List<Person> findByAgeLessThanOrderByAge(Integer age);
 
-    public List<?> getPersonsByCity(String city){
-        return (List<?>) entityManager.createQuery("SELECT person from Person person where cityOfLiving = ?1")
-                .setParameter(1, city.toUpperCase()).getResultList();
-    }
+    List<Person> findByNameAndSurnameLike(String name, String surname, Optional<Person> optional);
+
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    public List<?> getPersonsByCity(String city){
+//        return (List<?>) entityManager.createQuery("SELECT person from Person person where cityOfLiving = ?1")
+//                .setParameter(1, city.toUpperCase()).getResultList();
+//    }
 
 }
