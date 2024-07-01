9c2a5b68a083cd6b784581d6e763ee5f51cf6e18
Shaa test <shvyrev.anton78@gmail.com>
2024-07-01 18:24:11 +0500

    addSeduredMethods

--- src/main/java/ru/netology/sql_hibirnate/controller/Controller.java
+++ src/main/java/ru/netology/sql_hibirnate/controller/Controller.java
@@ -1,6 +1,10 @@
 package ru.netology.sql_hibirnate.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +26,9 @@ public class Controller {
         this.repository = repository;
     }
 
+
     @GetMapping("/by-id")
+    @Secured({"ROLE_READ"})
     public Person findById(String name, String surname, Integer age){
         CompositeKey compositeKey = new CompositeKey();
         compositeKey.setName(name);
@@ -31,16 +37,24 @@ public class Controller {
         return repository.findById(compositeKey).orElse(null);
     }
 
+    @PostAuthorize("hasAnyRole('WRITE', 'DELETE')")
     @GetMapping("/by-city")
     public List<Person> getPersonsByCity(String city){
         List<?> persons = repository.findByCityOfLiving(city);
         return (List<Person>) persons;
     }
 
+
     @GetMapping("/less-age")
+    @RolesAllowed({"ROLE_WRITE"})
     public List<Person> getPersonsLessAge(Integer age){
         return repository.findByIdAgeLessThanOrderByIdAge(age);
+    }
 
+    @GetMapping("/by-name")
+    @PreAuthorize("#name == authentication.principal.username")
+    public List<Person> getPersonsByName(String name){
+        return repository.findByIdName(name);
     }
 
     @GetMapping("/by-name-surname")
--- src/main/resources/templates/scema.sql
+++ src/main/resources/templates/scema.sql
@@ -1,1 +1,4 @@
 create schema public;
+
+insert into public.PERSONS(name, surname, age, phone_number, city_of_living)
+values('anton', 'sverlov','34','+79196892583','PERM');
